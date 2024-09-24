f6f46f3b09bc0354e84d7ac8e59f0d1bd780d1fa
Ecotech <e107ti@etec.sp.gov.br>
2024-09-23 21:31:26 -0300

    botao de enter alterado

--- back.tcc/home/home.js
+++
@@ -1,6 +0,0 @@
-const map = L.map('map').setView([-24.10508, -46.650696], 11);
-
-	const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		maxZoom: 50,
-		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-	}).addTo(map);
